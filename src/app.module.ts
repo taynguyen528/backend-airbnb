98aa6ab83d70907443c0c3a80c02f4f363758bb4
Tay Nguyen <pttnguyen528@gmail.com>
2024-07-03 01:05:17 +0700

    config prisma, db, .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    PrismaModule.forRootAsync({
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => {
+        return {
+          prismaOptions: {
+            log: [configService.get('log') || 'query', 'info', 'warn', 'error'],
+            datasources: {
+              db: {
+                url: configService.get('DATABASE_URL'),
+              },
+            },
+          },
+          explicitConnect: configService.get('explicit'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  const configService = app.get(ConfigService);
+
+  // config cors
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+  });
+
+  // config version API
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'], //v1, v2
+  });
+
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();

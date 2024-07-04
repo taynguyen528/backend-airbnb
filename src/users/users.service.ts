2c125c535ef15e8b92c7aa4e203dcc176dbe3941
taynguyen528 <pttnguyen528@gmail.com>
2024-07-04 09:07:20 +0700

    push

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,26 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto } from './dto/create-user.dto';
-
+import { genSaltSync, hashSync } from 'bcrypt';
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User)
-    private readonly userRepository: typeof User,
+    private readonly userModel: typeof User,
   ) {}
 
   async findAll(): Promise<User[]> {
-    return this.userRepository.findAll();
+    return this.userModel.findAll();
   }
+
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+
+    return hash;
+  };
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, passWord, phoneNumber, birthDay, gender, userType } =
       createUserDto;
 
-    let newUser = await this.userRepository.create({
+    const hashPassword = this.getHashPassword(passWord);
+    const newUser = await this.userModel.create({
       name,
       email,
-      passWord,
+      passWord: hashPassword,
       phoneNumber,
       birthDay,
       gender,
